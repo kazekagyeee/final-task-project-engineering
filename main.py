5425df3f567c8ca3892fb0fa5cbe9b0feda03d0c
kriskriskris0 <a7771977@gmail.com>
2025-05-04 17:38:07 +0500

    ssylka ne ponravilas

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import webbrowser
 import time
 import pyautogui
 
-url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+url = "https://turkish-tv-series.tv/turkey-serials/22792-klyukvennyy-scherbet7-vse-srii-smotret-tureckiiy-serial-na-russkom-yazyke4/99-seria.html"
 webbrowser.open(url)
 
-time.sleep(3)
+time.sleep(5)
 
 pyautogui.click(x=500, y=500)
